b51b65bf2f126ad6791ae27bfab64f3337d5905b
Hrisitjan Stojcevski <kiksa.bt@gmail.com>
2020-04-08 21:50:15 +0200

    FirstSpringCommit
    Commit from github desktop

---
+++ spring/src/main/java/mk/ukim/finki/wp/rentscoot/RentScootApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.rentscoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentScootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentScootApplication.class, args);
+    }
+
+}
---
+++ spring/src/test/java/mk/ukim/finki/wp/rentscoot/RentScootApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.rentscoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentScootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
